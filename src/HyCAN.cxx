1d497160041c9f43196eebf8dc23c0ae85c68e44
MoonFeather <moonbite233@gmail.com>
2025-05-25 13:02:30 +0800

    Refactor CMakeLists.txt to use glob for source files and add interface down functionality in HyCAN

--- src/HyCAN.cxx
+++ src/HyCAN.cxx
@@ -11,4 +11,9 @@ export void init()
         std::cerr << "Failed to set up interface. Details: " << result.error() <<
             std::endl;
     }
+    if (const auto result = set_interface_down("vcan0"); !result)
+    {
+        std::cerr << "Failed to set down interface. Details: " << result.error() <<
+            std::endl;
+    }
 }
