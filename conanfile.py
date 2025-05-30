27d8cc8197ffa557064217dc72a090571081f805
MoonFeather <moonbite233@gmail.com>
2025-05-30 22:58:03 +0800

    refactor: update conanfile and version to 0.3.1

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,12 @@
 import os
 
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import copy
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 
 
-class HyCANRecipe(ConanFile):
-    name = "HyCAN"
-    version = "0.3.0"
-    license = "BSD-3-Clause"
-    author = "MoonFeather moonfeather120@outlook.com"
-    url = "https://github.com/RoboMaster-DLMU-CONE/HyCAN"
-    description = "Modern high-performance Linux C++ CAN communication protocol library"
-    topics = ("canbus", "linux", "c++20", "network")
+class Conan(ConanFile):
+    name = "hycan"
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = "CMakeLists.txt", "include/*", "example/*", "README.md"
-
-    def layout(self):
-        cmake_layout(self)
 
     def requirements(self):
         self.requires("xtr/2.1.2")
@@ -26,22 +15,5 @@ class HyCANRecipe(ConanFile):
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
-
         tc = CMakeToolchain(self)
-        tc.variables["CMAKE_CXX_STANDARD"] = "23"
         tc.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        copy(self, "README.md", self.source_folder, self.package_folder)
-
-    def package_info(self):
-        self.cpp_info.libs = ["HyCAN"]
